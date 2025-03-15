8a271281907b0202fbc40b4f98933f08b37065ee
Almog Tavor <almogtavor@gmail.com>
2025-03-16 01:20:15 +0200

    chore: WIP make cc standalone work

--- symnmf.c
+++ symnmf.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "symnmf.h"
 
 #define EPSILON 1e-4
 #define MAX_ITER 300
 #define BETA 0.5
+
+#define MAX_LINE 1024
+#define ERROR_MSG "An Error Has Occurred\n"
+
+double** sym(double** x_matrix, int n, int d);
+double** ddg(double** a_matrix, int n);
+double** norm(double** a_matrix, double** d_matrix, int n);
+double** symnmf(double** w_matrix, double** h_matrix, int n, int k);
 
 double calc_distance(double *point1, double *point2, int cords_num) {
     double sum = 0;
@@ -26,11 +33,22 @@ double** sym(double** x_matrix, int n, int d) {
         a_matrix[i] = calloc(n, sizeof(double));
         for (j = 0; j < n; j++) {
             if (i != j) {
+                double bla = -calc_distance(x_matrix[i], x_matrix[j], d);
+                double ex = exp(-calc_distance(x_matrix[i], x_matrix[j], d) / 2.0);
+                printf("before exp: %f \n", bla);
+                printf("exp: %f \n", ex);
                 a_matrix[i][j] = exp(-calc_distance(x_matrix[i], x_matrix[j], d) / 2.0);
             } else {
                 a_matrix[i][j] = 0.0;
             }
         }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < d; j++) {
+            printf("%.4f", a_matrix[i][j]);
+            if (j < d - 1) printf(",");
+        }
+        printf("\n");
     }
     return a_matrix;
 }
@@ -169,14 +187,14 @@ void free_matrix(double** matrix, int n) {
 /* SymNMF Main Function */
 double** symnmf(double** w_matrix, double** h_matrix, int n, int k) {
     double** new_h;
-    int* clusters;
+/*    int* clusters;*/
     int iter, i, j;
 
     for (iter = 0; iter < MAX_ITER; iter++) {
         new_h = update_H(w_matrix, h_matrix, n, k);
 
         if (has_converged(h_matrix, new_h, n, k)) {
-            free_matrix(new_h, n); // Free new_h since it's unused
+            free_matrix(new_h, n);
             break;
         }
 
@@ -186,15 +204,157 @@ double** symnmf(double** w_matrix, double** h_matrix, int n, int k) {
                 h_matrix[i][j] = new_h[i][j];
             }
         }
-
-        free_matrix(new_h, n); // Free the old `new_h` after copying its content
+        /* Free the old `new_h` after copying its content */
+        free_matrix(new_h, n);
     }
 
-//    clusters = derive_clustering(h_matrix, n, k);
+/*    clusters = derive_clustering(h_matrix, n, k); */
 
-//    printf("Clustering Results:\n");
-//    print_clusters(clusters, n);
+/*    printf("Clustering Results:\n"); */
+/*    print_clusters(clusters, n); */
 
-//    free(clusters);
+/*    free(clusters); */
     return h_matrix;
+}
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "symnmf.h"
+
+#define MAX_LINE 1024
+#define ERROR_MSG "An Error Has Occurred\n"
+
+/* Function to read data points from file */
+double** read_data(const char* file_name, int* n, int* d) {
+    FILE* file;
+    double** data;
+    char line[MAX_LINE];
+    int row, col, i;
+    char* token;
+
+    file = fopen(file_name, "r");
+    if (!file) {
+        printf(ERROR_MSG);
+        exit(1);
+    }
+
+    data = NULL;
+    row = 0;
+    *d = 0;
+
+    while (fgets(line, MAX_LINE, file)) {
+        if (row == 0) {
+            /* Count dimensions */
+            token = strtok(line, ",");
+            while (token) {
+                (*d)++;
+                token = strtok(NULL, ",");
+            }
+        }
+
+        data = realloc(data, (row + 1) * sizeof(double*));
+        if (!data) {
+            printf(ERROR_MSG);
+            exit(1);
+        }
+
+        data[row] = malloc((*d) * sizeof(double));
+        if (!data[row]) {
+            printf(ERROR_MSG);
+            exit(1);
+        }
+
+        /* Re-read the first tokenized line using fgets again */
+        fseek(file, 0, SEEK_SET);  /* Reset file pointer to the start */
+        for (i = 0; i <= row; i++) {
+            fgets(line, MAX_LINE, file);
+        }
+
+        token = strtok(line, ",");
+        for (col = 0; col < *d; col++) {
+            if (token) {
+                data[row][col] = atof(token);
+                token = strtok(NULL, ",");
+            }
+        }
+
+        row++;
+    }
+
+    *n = row;
+    fclose(file);
+    return data;
+}
+
+/* Main Function for Handling Arguments and Goals */
+int main(int argc, char* argv[]) {
+    int i, j, n, d;
+    double** data;
+    double** result;
+    double** a_matrix;
+    double** d_matrix;
+
+    if (argc != 3) {
+        printf(ERROR_MSG);
+        return 1;
+    }
+
+    data = read_data(argv[2], &n, &d);
+
+    if (strcmp(argv[1], "sym") == 0) {
+        result = sym(data, n, d);
+
+    } else if (strcmp(argv[1], "ddg") == 0) {
+        a_matrix = sym(data, n, d);
+        result = ddg(a_matrix, n);
+
+        /* Free a_matrix since it's only needed here */
+        for (i = 0; i < n; i++) {
+            free(a_matrix[i]);
+        }
+        free(a_matrix);
+
+    } else if (strcmp(argv[1], "norm") == 0) {
+        a_matrix = sym(data, n, d);
+        d_matrix = ddg(a_matrix, n);
+        result = norm(a_matrix, d_matrix, n);
+
+        /* Free a_matrix and d_matrix since they're only needed here */
+        for (i = 0; i < n; i++) {
+            free(a_matrix[i]);
+            free(d_matrix[i]);
+        }
+        free(a_matrix);
+        free(d_matrix);
+
+    } else {
+        printf(ERROR_MSG);
+        return 1;
+    }
+
+    /* Print results */
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < d; j++) {
+            printf("%.4f", result[i][j]);
+            if (j < d - 1) printf(",");
+        }
+        printf("\n");
+    }
+
+    /* Free allocated memory */
+    for (i = 0; i < n; i++) {
+        free(data[i]);
+        free(result[i]);
+    }
+    free(data);
+    free(result);
+
+    return 0;
 }
